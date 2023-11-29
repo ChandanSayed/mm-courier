656eceade40f21127247ebb2e92c0ef4c18407ff
Chandan Sayed <hangzhou635@gmail.com>
2023-11-29 20:46:57 +0600

    Project initialized

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import AppContext from './context/AppContext.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppContext>
+      <RouterProvider router={router} />
+    </AppContext>
+  </React.StrictMode>
+);
